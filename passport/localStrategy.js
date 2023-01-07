3c415f10a6dc6f34f1a91831aafde9b32ea12c45
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-01-07 19:35:07 +0900

    feat: 로컬 로그인을 위한 passport local 작성

---
+++ passport/localStrategy.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, async (email, password, done) => {
+    try {
+    const user = await User.findOne({
+        where: {
+            email
+        }
+    });
+    if (user) {
+        const result = await bcrypt.compare(password, user.password);
+        if (result) {
+            done(null, user);
+        } else {
+            done(null, false, {
+            message: '❌ 잘못된 비밀번호입니다 ❌'
+            });
+        }
+        } else {
+            done(null, false, {
+                message: '❌ 가입되지 않은 회원입니다 ❌'
+            });
+        }
+        } catch (error) {
+            console.error(error);
+            done(error);
+        }
+    }));
+};
