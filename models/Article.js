1e72acd1e07d61878ed3acbb03e0777bf2d2e6ba
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-01-07 19:05:07 +0900

    feat: Sequelize 모델 정의 User, Article, ArticleGroup ArticleGroup 자동 미생성으로 확인 필요

---
+++ models/Article.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+module.exports = class Article extends Sequelize.Model {
+static init(sequelize) {
+    return super.init({
+    articleId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey:true,
+    autoIncrement:true,
+    },
+    articleHeader: {
+    type: Sequelize.STRING(200),
+    allowNull: false,
+    validate:{
+        len:[1, 200]
+    }
+    },
+    articleBody: {
+    type: Sequelize.STRING(4096),
+    allowNull: false,
+    validate:{
+        len:[1, 1024]
+    }
+    },
+    articleReaction:{
+        type:Sequelize.STRING(1020),
+        allowNull:false,
+        validate:{
+            is:/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        }
+    }
+    }, {
+        sequelize,
+        timestamps: true,
+        paranoid: true,
+        modelName: 'Article',
+        tableName: 'Article',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+        });
+}
+static associate(db) {
+    db.Article.belongsTo(db.User,
+        {foreignKey:"fk_user_article", targetKey:"userId"});
+    db.Article.belongsTo(db.ArticleGroup,{
+        foreignKey:"fk_article_group", targetKey:"groupId"
+    });
+}
+};
---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { UUIDV4 } = require('sequelize');
+const Sequelize = require('sequelize');
+module.exports = class User extends Sequelize.Model {
+static init(sequelize) {
+    return super.init({
+    userId: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey:true,
+    defaultValue:UUIDV4,
+    },
+    nickname: {
+    type: Sequelize.STRING(150),
+    allowNull: false,
+    unique:true,
+    validate:{
+        //닉네임은 3자 이상 15자 이하
+        //영어 대소문자와 숫자, 한글만 가능
+        is:/^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{3,15}$/,
+        len:[3, 15]
+    }
+    },
+    password: {
+    type: Sequelize.STRING(256),
+    allowNull: false,
+    },
+    socialLogin:{
+        type:Sequelize.BOOLEAN,
+        allowNull:false,
+        defaultValue:false,
+    },
+    cashed:{
+        type:Sequelize.BOOLEAN,
+        allowNull:false,
+        defaultValue:false,
+    },
+    cahsedDate:{
+        type:Sequelize.DATE,
+        allowNull:true,
+        defaultValue:null,
+        validate:{
+            isDate:true,
+        }
+    }
+    }, {
+        sequelize,
+        timestamps: true,
+        paranoid: true,
+        modelName: 'User',
+        tableName: 'User',
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        });
+}
+static associate(db) {
+    db.User.hasMany(db.Article,{
+        foreignKey:"fk_user_article",
+        sourceKey:"userId"
+    })
+    }
+};
