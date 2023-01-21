2a0060fd2bd3b6d45c1b30ed5753ebbea1063644
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-01-22 00:24:34 +0900

    remove: sequelize 모델 추가 reaction은 일 대 다 관계로 모델 추가 생성

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+module.exports = class Reaction extends Sequelize.Model {
+static init(sequelize) {
+    return super.init({
+        reactionId : {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        reactionContent:{
+            type:Sequelize.STRING(1020),
+            allowNull:false,
+            validate:{
+                is:/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+            }
+        }
+    }, {
+        sequelize,
+        timestamps: true,
+        paranoid: true,
+        modelName: 'Reaction',
+        tableName: 'Reaction',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+        });
+}
+static associate(db) {
+    db.Reaction.belongsTo(db.Article,
+        {foreignKey:"fk_article_reaction", targetKey:"articleId"});
+}
+};
