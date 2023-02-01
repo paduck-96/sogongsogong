6698465084a6528d2b5a1654b2e431949cb2ab06
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-02-01 20:25:23 +0900

    feat: 게시글 수정 페이지

---
+++ client/src/components/Update.jsx
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+//import {Navigate} from 'react-router-dom'
+
+const Update = () => {
+    const params = useParams();
+    const navigate = useNavigate();
+    const [articleInfo, setArticleInfo] = useState({
+        articleTitle:"",
+        articleContent:"",
+        categoryName:""
+    })
+    useEffect(()=>{
+            fetch(`http://localhost/article/${params.articleId}`,{
+                headers:{
+                    "Content-Type":"application/json",
+                }
+            })
+            .then(res=>res.json())
+            .then(res=>{
+                setArticleInfo({
+                    articleTitle:res.article.articleTitle,
+                    articleContent:res.article.articleContent,
+                    categoryName:res.category.join(",")
+                })
+            })
+    }, [params.articleId])
+    const onChangeHandler = (e) => {
+        const newArticleInfo = {
+                    ...articleInfo,
+                    [e.target.name] : e.target.value
+                };
+                setArticleInfo(newArticleInfo);
+    };
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        if(articleInfo.articleTitle.trim() === "" || articleInfo.articleContent.trim() === ""){
+            alert("내용 입력 필수");
+        }
+        const response = await fetch(`http://localhost/article/${params.articleId}/update`, {
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                articleTitle:articleInfo.articleTitle,
+                articleContent:articleInfo.articleContent,
+            })
+        }).then(res=>res.json());
+        if(response.result ==="success"){
+            return navigate("/articles", {replace:true})
+        }
+    }
+    return (
+        <div>
+            <form method="put" onSubmit={onSubmitHandler}>
+                <label>
+                    제목 :
+                    <input type="text" name='articleTitle' value={articleInfo.articleTitle} onChange={onChangeHandler} />
+                </label>
+                <br />
+                <label>
+                    카테고리 :
+                    <input type="text"  name='categoryName' value={articleInfo.categoryName} onChange={onChangeHandler} disabled/>
+                </label>
+                <br/>
+                <label>
+                    내용 :
+                    <textarea name='articleContent' value={articleInfo.articleContent} onChange={onChangeHandler} />
+                </label>
+                <br />
+                <button type='submit'>글 수정</button>
+            </form>
+        </div>
+    );
+};
+
+export default Update;
