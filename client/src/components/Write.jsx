a84110f2574a9f25eec7549c8e2f47307fcfba56
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-02-01 20:24:05 +0900

    feat: 게시글 작성 페이지

--- client/src/components/Write.jsx
+++ client/src/components/Write.jsx
@@ -1,16 +1,61 @@
-import React from 'react';
-import {Navigate} from 'react-router-dom'
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+//import {Navigate} from 'react-router-dom'
 
 const Write = () => {
-    //const params = useParams();
-    //const memberId = params.memberId;
-    const isLoggedIn = false;
-    if(!isLoggedIn){
-        return <Navigate to="/login" replace={true}/>
+    const navigate = useNavigate();
+    const [articleInfo, setArticleInfo] = useState({
+        articleTitle:"",
+        articleContent:"",
+        categoryName:"미정"
+    });
+    const onChangeHandler = (e) => {
+        const newArticleInfo = {
+                    ...articleInfo,
+                    [e.target.name] : e.target.value
+                };
+                setArticleInfo(newArticleInfo);
+    };
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        if(articleInfo.articleTitle.trim() === "" || articleInfo.articleContent.trim() === ""){
+            alert("내용 입력 필수");
+        }
+        const response = await fetch("http://localhost/article", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                articleTitle:articleInfo.articleTitle,
+                articleContent:articleInfo.articleContent,
+                categoryName:articleInfo.categoryName,
+            })
+        }).then(res=>res.json());
+        if(response.result ==="success"){
+            return navigate("/articles", {replace:true})
+        }
     }
     return (
         <div>
-            
+            <form method="post" onSubmit={onSubmitHandler}>
+                <label>
+                    제목 :
+                    <input type="text" name='articleTitle' value={articleInfo.articleTitle} onChange={onChangeHandler} />
+                </label>
+                <br />
+                <label>
+                    카테고리 :
+                    <input type="text"  name='categoryName' value={articleInfo.categoryName} onChange={onChangeHandler} />
+                </label>
+                <br/>
+                <label>
+                    내용 :
+                    <textarea name='articleContent' value={articleInfo.articleContent} onChange={onChangeHandler} />
+                </label>
+                <br />
+                <button type='submit'>글 작성</button>
+            </form>
         </div>
     );
 };
