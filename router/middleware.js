53688ce6519863eb95f2c6d797f0219e4ca1d020
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-02-07 16:26:00 +0900

    feat: 인증 미들웨어

--- router/middleware.js
+++ router/middleware.js
@@ -1,38 +1,26 @@
+const jwt = require("jsonwebtoken");
 
-exports.isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        //res.redirect('/?loginError=로그인이 필요합니다.');
-        res.status(403).json({result:"Fail", message:"로그인 필요"});
+exports.isAuth = (req, res, next) => {
+    const authHeader = req.get("Authorization");
+    console.log(authHeader);
+    if(!authHeader){
+        const err = new Error("인증이 필요합니다");
+        err.statusCode = 401;
+        throw err;
     }
-    };
+    const token = req.get("Authorization").split(" ")[1];
+    try{
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-exports.isNotLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        next();
-    } else {
-        const message = encodeURIComponent("로그인 상태입니다");
-        res.status(302).redirect(`/?error=${message}`);
+    }catch(err){
+        err.statusCode = 500;
+        throw err;
     }
-};
-
-// JWT
-const jwt = require('jsonwebtoken');
-exports.verifyToken = (req, res, next) => {
-    try {
-        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-        return next();
-    } catch (error) {
-        if (error.name === 'TokenExpiredError') { 
-        return res.status(419).json({
-            code: 419,
-            message: '토큰이 만료되었습니다',
-            });
+    if(!decodedToken){
+        const err = new Error("인증 실패");
+        err.statusCode = 401;
+        throw err;
     }
-    return res.status(401).json({
-        code: 401,
-        message: '유효하지 않은 토큰입니다',
-    });
-    }
-};
+    req.userId = decodedToken.userId;
+    next();
+}
