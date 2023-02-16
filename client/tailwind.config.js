851a3953caf4edcb531b21f58c4cf4acc9d51dc2
paduck-96 <peterrabitt1234@naver.com>
2023-02-16 22:28:54 +0900

    feat: 메인페이지 CSS 작업 tailwindcss 활용을 위해 구조 변경

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "green1":"#E1F1AC",
+        "green2":"#C4F78D",
+        "green3":"#92F02D",
+        "green4":"#71CD0E",
+        "green5":"#5CA70C",
+        "green6":"#447C09"
+      }
+    },
+    fontFamily:{
+      "base":['Gugi', "cursive"]
+    }
   },
   plugins: [],
 }
